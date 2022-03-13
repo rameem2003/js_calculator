a2b8653ae3ea3fa7a959e931b2eabad7f85464bf
rameem2003 <rameem2019@gmail.com>
2022-03-13 07:33:58 +0600

    bug isn't fixed

--- app.js
+++ app.js
@@ -1,34 +1,84 @@
-function calcu(){
+// function calcu(){
+//     var num1 = parseInt(document.getElementById("num1").value);
+//     var num2 = parseInt(document.getElementById("num2").value);
+
+
+
+//     var output = document.getElementById("result");
+
+
+//     var op = document.getElementById("op").value;
+
+
+//     if(num1 == null && num2 == null){
+//         output.innerHTML = "Input empty";
+//     }
+//     else{
+//         switch(op){
+//             case "add":
+//                 var add = num1 + num2;
+//                 output.innerHTML = "The result of " + num1 + "+" + num2 + " is " + add;
+//                 break;
+    
+//             case "sub":
+//                 var sub = num1 - num2;
+//                 output.innerHTML = "The result of " + num1 + "-" + num2 + " is " + sub;
+//                 break;
+    
+//             case "mul":
+//                 var mul = num1 * num2;
+//                 output.innerHTML = "The result of " + num1 + "*" + num2 + " is " + mul;
+//                 break;
+    
+//             case "div":
+//                 var div = num1 / num2;
+//                 output.innerHTML = "The result of " + num1 + "/" + num2 + " is " + div;
+//                 break;
+    
+//             default:
+//                 output.innerHTML = "Invalide!!";
+//                 break;
+//         }
+//     }
+// }
+
+
+var run = document.getElementById("run");
+
+
+run.addEventListener("click", ()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
-
-
-
+    var op = document.getElementById("op").value;
     var output = document.getElementById("result");
 
-
-    var op = document.getElementById("op").value;
-
-
-    switch(op){
-        case "add":
-            var add = num1 + num2;
-            output.innerHTML = "The result of " + num1 + "+" + num2 + " is " + add;
-            break;
-
-        case "sub":
-            var sub = num1 - num2;
-            output.innerHTML = "The result of " + num1 + "-" + num2 + " is " + sub;
-            break;
-
-        case "mul":
-            var mul = num1 * num2;
-            output.innerHTML = "The result of " + num1 + "*" + num2 + " is " + mul;
-            break;
-
-        case "div":
-            var div = num1 / num2;
-            output.innerHTML = "The result of " + num1 + "/" + num2 + " is " + div;
-            break;
+    if(num1 != "" && num2 != ""){
+        switch(op){
+            case "add":
+                var add = num1 + num2;
+                output.innerHTML = "The result of " + num1 + "+" + num2 + " is " + add;
+                break;
+    
+            case "sub":
+                var sub = num1 - num2;
+                output.innerHTML = "The result of " + num1 + "-" + num2 + " is " + sub;
+                break;
+    
+            case "mul":
+                var mul = num1 * num2;
+                output.innerHTML = "The result of " + num1 + "*" + num2 + " is " + mul;
+                break;
+    
+            case "div":
+                var div = num1 / num2;
+                output.innerHTML = "The result of " + num1 + "/" + num2 + " is " + div;
+                break;
+    
+            default:
+                output.innerHTML = "Invalide!!";
+                break;
+        }
+    }else{
+        output.innerHTML = "Input empty";
     }
-}
+})
